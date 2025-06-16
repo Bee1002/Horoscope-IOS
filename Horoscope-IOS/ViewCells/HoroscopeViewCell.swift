a37425398b2753f159dcac6318e5a5f16b9d9b5b
Bee1002 <alvaroslim@gmail.com>
2025-06-16 19:11:47 +0200

    agregados imagenes y horoscopos

---
+++ Horoscope-IOS/ViewCells/HoroscopeViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  HoroscopeViewCell.swift
+//  Horoscope-IOS
+//
+//  Created by Tardes on 16/6/25.
+//
+
+import UIKit
+
+class HoroscopeViewCell: UITableViewCell {
+    
+    @IBOutlet weak var horoscopeImageView: UIImageView!
+    @IBOutlet weak var horoscopeNameLabel: UILabel!
+    @IBOutlet weak var horoscopeDatesLabel: UILabel!
+    
+    func render (horoscope: Horoscope) {
+        horoscopeNameLabel.text = horoscope.name
+        horoscopeDatesLabel.text = horoscope.dates
+        horoscopeImageView.image = horoscope.getImage()
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
